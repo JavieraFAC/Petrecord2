f32d6355ce1926f215214a9151ee466f33220536
JavieraFAC <ja.alvarezc@duocuc.cl>
2024-11-14 14:44:54 -0300

    arreglo

--- src/app/pages/Usuario/perfil/perfil.page.ts
+++ src/app/pages/Usuario/perfil/perfil.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/services/firebase.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  nombre: string = '';
+  apellidos: string = '';
+  run: string = '';
+
+
+  constructor(private firebaseService: FirebaseService, private auth: AngularFireAuth) {}
 
   ngOnInit() {
+    this.auth.currentUser.then(user => {
+      if (user) {
+        // Llamamos a la función para obtener los datos del usuario desde Firestore
+        this.firebaseService.getUserData(user.uid).then(data => {
+          if (data) {
+            this.nombre = data['nombre'] || '';
+            this.run = data['run'] || '';
+            this.apellidos = data['apellidos'];
+          }
+        });
+      }
+    });
   }
-
 }
